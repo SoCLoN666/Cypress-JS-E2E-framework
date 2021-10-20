9c8fd9ecca33ca519436384e2e4464c6b6b384a2
okusmin <okusmin@questrade.com>
2021-10-20 17:08:36 +0300

    Add yaml for github action sync

--- cypress/integration/swag-labs-login.js
+++ cypress/integration/swag-labs-login.js
@@ -10,22 +10,8 @@ describe('When visiting SwagLab login page', () => {
     it('should see the correct SwagLab URL', () => {
         test.shouldBeOpened('eq', 'https://www.saucedemo.com/');
     });
-    // it('should be able to succesfully login', () => {
-    //     test.get('#text').should($text => {
-    //         const txt = $text.text();
-    //         console.log(txt);
-    //     });
-    //     // console.log(a);
-    // });
-    //         it('test', () => {
-    //             cy.xpath('//*[@id="login_credentials"]/text()[1]').should($login_credentials => {
-    //                 const text = $login_credentials.text();
-    //             cy.get('#user-name').type(text).should('have.value', text)
-    // });
+
     it('test', () => {
-        // cy.get('#user-name').type(name).should('have.value', name);
-        // cy.get('#password').type(pass).should('have.value', pass);
-        // cy.get('#login-button').click();
         const username = 'standard_user';
         const password = 'secret_sauce';
         test.typeUserName(username);
